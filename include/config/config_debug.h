3c3f232656dd8f2d810a59be1b95064775f7895b
aglab2 <aglab3@gmail.com>
2023-08-21 21:54:28 +0800

    Textures

--- include/config/config_debug.h
+++ include/config/config_debug.h
@@ -26,12 +26,12 @@
  * This allows you to quickly test the level you're working on.
  * If you want the game to boot normally, just comment out the define again.
  */
-#define TEST_LEVEL LEVEL_BOB
+// #define TEST_LEVEL LEVEL_BOB
 
 /**
  * Enables debug level select. Hold L while the game boots to turn it on.
  */
-#define DEBUG_LEVEL_SELECT
+// #define DEBUG_LEVEL_SELECT
 
 /**
  * Enables debug free move (D-pad up to enter, A to exit).
