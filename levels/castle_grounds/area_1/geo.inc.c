3f53a6f8f924c1b0d2c6c1786212b83e9410a693
aglab2 <aglab3@gmail.com>
2023-08-21 09:19:26 +0800

    ow1, c2 improvements, no AD

--- levels/castle_grounds/area_1/geo.inc.c
+++ levels/castle_grounds/area_1/geo.inc.c
@@ -4,7 +4,7 @@ const GeoLayout castle_grounds_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
 		GEO_ROTATION_NODE_WITH_DL(LAYER_OPAQUE, 90, 0, 0, castle_grounds_dl_DL_castle_grounds_1_0xe0258c0_Obj_mesh_layer_1),
-		GEO_ROTATION_NODE_WITH_DL(LAYER_TRANSPARENT, 90, 0, 0, castle_grounds_dl_DL_castle_grounds_1_0xe02a308_Obj_mesh_layer_5),
+		GEO_ROTATION_NODE_WITH_DL(LAYER_ALPHA, 90, 0, 0, castle_grounds_dl_DL_castle_grounds_1_0xe02a308_Obj_mesh_layer_4),
 		GEO_ROTATION_NODE_WITH_DL(LAYER_ALPHA, 90, 0, 0, castle_grounds_dl_DL_castle_grounds_1_0xe02a8d0_Obj_mesh_layer_4),
 		GEO_ASM(0, geo_cannon_circle_base),
 	GEO_CLOSE_NODE(),
